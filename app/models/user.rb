3e0dad026f082d93a2452038944441ff1cb31998
Mads Balslev <madspbalslev@gmail.com>
2024-10-02 11:10:25 +0200

    view stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :documents, dependent: :destroy
 
+  has_one_attached :avatar
+
   validates :first_name, presence: true
   validates :last_name, presence: true
 
